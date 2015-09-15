3adcbc90e01e3b5a3881784d2fe3e05bcedb4416
Anthony Ccapira Acvendaño <anthony.ccapira@ucsp.edu.pe>
2015-09-15 10:59:26 -0500

    Se añadio gitignore y se eliminó archivos de compilación

--- utils/PID.py
+++ utils/PID.py
@@ -1,11 +1,23 @@
 class PID:
+    """ La clase PID, implementa un mecanismo de control, Proportional, Integral Derivative controller
+        Se basa en el cálculo de los errores entre los valores medidos y los valores
+        deseados.
+    """
     def __init__(self, kp, kd, ki):
         self.Kp = kp
         self.Kd = kd
         self.Ki = ki
         self.prev_error = 0
         self.cum_error = 0
-
+        """
+            KP:
+                constante proporcional
+            Kd:
+                constante derivativa
+            Ki:
+                constante integral
+        """
+    #salida: suma de la parte proporcional, derivativa e integral.
     def update(self, error):
         P = self.Kp * error
         D = self.Kd * (error - self.prev_error)
--- utils/core.py
+++ utils/core.py
@@ -3,7 +3,6 @@ import numpy as np
 RED_TEAM = 0
 BLUE_TEAM = 1
 
-
 def normalize_angle(angle):
     while angle > 180:
         angle -= 360
@@ -13,24 +12,32 @@ def normalize_angle(angle):
 
 
 class Position:
+    """ Clase posición, contiene funciones para calcular la distancia hacia otra posición
+        También podemos calcular el ángulo de orientación normalizado.
+    """
     def __init__(self, x=0, y=0, theta=0):
+        """ x and y:
+                Coordenadas de la posición actual del objeto.
+            theta:
+                Es el ángulo de orientación del objeto (Robots y balón).
+        """
         self.x = x
         self.y = y
         self.theta = normalize_angle(theta)
-
+    #to print
     def __str__(self):
         return 'x: %s, y: %s, theta: %s' % (self.x, self.y, self.theta)
 
     def vector_to(self, position):
         return np.array([position.x, position.y]) - np.array([self.x, self.y])
-
+    #trasladar el objeto
     def translate(self, vec):
         return Position(self.x + vec[0], self.y + vec[1])
-
+    #distancia entre dos puntos usando euclides
     def distance_to(self, position):
         v = self.vector_to(position)
         return np.linalg.norm(v)
-
+    #get angle, if deg flag is set true = grades
     def angle_to(self, position, deg=True):
         v = self.vector_to(position)
         ang = np.arctan2(v[1], v[0])
