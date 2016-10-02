9ecf8db02ee26751d50184d161a11d4a072f3c7d
Jose Carrillo <quique0194@gmail.com>
2016-10-02 11:41:30 -0500

    Created setup.py

--- lib/setup.py
+++ lib/setup.py
@@ -5,9 +5,9 @@ from distutils.core import setup
 setup(
     name='umayux-strategy',
     version='1.0',
-    description='Very Small Size strategy framework',
+    description='Very Small Size strategy framework for Umayux team',
     author='Jose Enrique Carrillo Pino',
     author_email='quique0194@gmail.com',
     # url='https://www.python.org/sigs/distutils-sig/',
-    packages=['distutils', 'distutils.command'],
+    packages=['vsss'],
 )
